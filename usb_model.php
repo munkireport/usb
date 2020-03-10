1cb28177e7299a61a8c2b499f18e5a134ffeaf2c
tuxudo <tuxudo@users.noreply.github.com>
2020-03-10 09:30:05 -0400

    Added FaceTime camera

--- usb_model.php
+++ usb_model.php
@@ -24,10 +24,10 @@ class Usb_model extends \Model {
 
 		$this->serial_number = $serial;
 	}
-	
+
 	// ------------------------------------------------------------------------
 
-    
+
      /**
      * Get USB device names for widget
      *
@@ -50,7 +50,7 @@ class Usb_model extends \Model {
         }
         return $out;
      }
-    
+
      /**
      * Get USB device types for widget
      *
@@ -73,7 +73,7 @@ class Usb_model extends \Model {
         }
         return $out;
      }
-    
+
 	/**
 	 * Process data sent by postflight
 	 *
@@ -82,18 +82,18 @@ class Usb_model extends \Model {
 	 **/
 	function process($plist)
 	{
-		
+
 		if ( ! $plist){
 			throw new Exception("Error Processing Request: No property list found", 1);
 		}
-		
+
 		// Delete previous set        
 		$this->deleteWhere('serial_number=?', $this->serial_number);
 
 		$parser = new CFPropertyList();
 		$parser->parse($plist, CFPropertyList::FORMAT_XML);
 		$myList = $parser->toArray();
-        		
+
 		$typeList = array(
 			'name' => '',
 			'type' => 'unknown', // Mouse, Trackpad, Hub, etc.
@@ -108,51 +108,51 @@ class Usb_model extends \Model {
 			'usb_serial_number' => '',
 			'printer_id' => ''
 		);
-		
+
 		foreach ($myList as $device) {
 			// Check if we have a name
 			if( ! array_key_exists("name", $device)){
 				continue;
 			}
-            
+
 			// Skip Bus types USB31Bus, USB11Bus, etc.
 			if(preg_match('/^USB(\d+)?Bus$/', $device['name']))
 			{
 				continue;
 			}
-			
+
 			// Check for USB bus devices and simulated USB devices to exclude
 			$excludeusb = array("OHCI Root Hub Simulation","UHCI Root Hub Simulation","EHCI Root Hub Simulation","RHCI Root Hub Simulation","XHCI Root Hub Simulation","XHCI Root Hub SS Simulation","XHCI Root Hub USB 2.0 Simulation");
 			if (in_array($device['name'], $excludeusb)) {
 				continue;
 			}
-            
+
 			// Skip internal devices if value is TRUE
 			if (!$this->config['usb_internal']) {
     			if ($device['internal']){
 					continue;
 				}
 			}
-			 
+
 			// Adjust names
 			$device['name'] = str_replace(array('bluetooth_device','hub_device','composite_device'), array('Bluetooth USB Host Controller','USB Hub','Composite Device'), $device['name']);
-            
+
 			// Adjust USB speeds
 			if (array_key_exists("device_speed",$device)) {
 				$device['device_speed'] = str_replace(array('low_speed','full_speed','high_speed','super_speed'), array('USB 1.0','USB 1.1','USB 2.0','USB 3.x'), $device['device_speed']);
 			} else {
 				$device['device_speed'] = 'USB 1.1';
 			}
-			
+
 			// Make sure manufacturer is set
 			$device['manufacturer'] = isset($device['manufacturer']) ? $device['manufacturer'] : '';
-            
+
 			// Make sure printer_id is set
 			$device['printer_id'] = isset($device['printer_id']) ? $device['printer_id'] : '';
-            
+
 			// Map name to device type
 			$device_types = array(
-				'Camera' => 'isight|camera|video',
+				'Camera' => 'isight|camera|video|facetime',
 				'USB Hub' => 'hub',
 				'Keyboard' => 'keyboard|keykoard|usb kb',
 				'IR Receiver' => 'ir receiver',
@@ -167,7 +167,7 @@ class Usb_model extends \Model {
 				'Network' => 'network|ethernet|modem|bcm',
 				'UPS' => 'ups',
 				'Audio Device' => 'audio|sound|headset|microphone|akm',
-				'TouchBar' => 'ibridge',
+				'iBridge' => 'ibridge',
 				'Scanner' => 'scanner',
 				'Wacom Tablet' => 'wacom|ptz-|intuos|ctl-',
 				'Interactive Board' => 'smartboard|activboard'
@@ -179,7 +179,7 @@ class Usb_model extends \Model {
 					break;
 				}
 			}
-			
+
 			// Set device types based on other criteria
 			if (stripos($device['manufacturer'], 'DisplayLink') !== false) {
 				$device['type'] = 'Display'; // Set by manufacturer instead of name
@@ -191,25 +191,25 @@ class Usb_model extends \Model {
 			if ($device['media'] == 1 ) {
 				$device['type'] = 'Mass Storage';
 			}
-                                    
+
 			// Override Internal T/F based on name
 			if (stripos($device['name'], 'Internal') !== false) {
 				$device['internal'] = 1;
 			}
-            
+
 			// Adjust Apple vendor ID
 			if (array_key_exists('vendor_id',$device)) {
 			if ($device['vendor_id'] == 'apple_vendor_id') {
 				$device['vendor_id'] = '0x05ac (Apple, Inc.)';
 			}
-            
+
 			// Set manufacturer from vendor ID if it's blank
 			if ($device['manufacturer'] == '') {
 				preg_match('/\((.*?)\)/s', $device['vendor_id'], $manufactureroutput);
 				$device['manufacturer'] = $manufactureroutput[1];
 			}
 		}
-            
+
 			foreach ($typeList as $key => $value) {
 				$this->rs[$key] = $value;
 				if(array_key_exists($key, $device))
@@ -217,7 +217,7 @@ class Usb_model extends \Model {
 					$this->rs[$key] = $device[$key];
 				}
 			}
-			
+
 			// Save device
 			$this->id = '';
 			$this->save();
