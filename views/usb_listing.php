4dd6201f7e5b206266c2e25c14cd1a6447f5a362
tuxudo <tuxudo@users.noreply.github.com>
2023-05-17 18:28:46 -0400

    v6 style

--- views/usb_listing.php
+++ views/usb_listing.php
@@ -1,9 +1,9 @@
 <?php $this->view('partials/head'); ?>
 
-<div class="container">
-  <div class="row">
+<div class="container-fluid">
+  <div class="row pt-4">
 	<div class="col-lg-12">
-	  <h3><span data-i18n="usb.reporttitle"></span> <span id="total-count" class='label label-primary'>…</span></h3>
+	  <h3><span data-i18n="usb.reporttitle"></span> <span id="total-count" class='badge badge-primary'>…</span></h3>
 	  <table class="table table-striped table-condensed table-bordered">
 		<thead>
 		  <tr>
@@ -40,71 +40,71 @@
 
 	$(document).on('appReady', function(e, lang) {
 
-        // Get modifiers from data attribute
-        var mySort = [], // Initial sort
-            hideThese = [], // Hidden columns
-            col = 0, // Column counter
-            runtypes = [], // Array for runtype column
-            columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
+		// Get modifiers from data attribute
+		var mySort = [], // Initial sort
+			hideThese = [], // Hidden columns
+			col = 0, // Column counter
+			runtypes = [], // Array for runtype column
+			columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
 
-        $('.table th').map(function(){
+		$('.table th').map(function(){
 
-            columnDefs.push({name: $(this).data('colname'), targets: col, render: $.fn.dataTable.render.text()});
+			columnDefs.push({name: $(this).data('colname'), targets: col, render: $.fn.dataTable.render.text()});
 
-            if($(this).data('sort')){
-              mySort.push([col, $(this).data('sort')])
-            }
+			if($(this).data('sort')){
+			  mySort.push([col, $(this).data('sort')])
+			}
 
-            if($(this).data('hide')){
-              hideThese.push(col);
-            }
+			if($(this).data('hide')){
+			  hideThese.push(col);
+			}
 
-            col++
-        });
+			col++
+		});
 
-	    oTable = $('.table').dataTable( {
-            ajax: {
-                url: appUrl + '/datatables/data',
-                type: "POST",
-                data: function(d){
-                     d.mrColNotEmpty = "name";
+		oTable = $('.table').dataTable( {
+			ajax: {
+				url: appUrl + '/datatables/data',
+				type: "POST",
+				data: function(d){
+					 d.mrColNotEmpty = "name";
 
-                    // Check for column in search
-                    if(d.search.value){
-                        $.each(d.columns, function(index, item){
-                            if(item.name == 'usb.' + d.search.value){
-                                d.columns[index].search.value = '> 0';
-                            }
-                        });
+					// Check for column in search
+					if(d.search.value){
+						$.each(d.columns, function(index, item){
+							if(item.name == 'usb.' + d.search.value){
+								d.columns[index].search.value = '> 0';
+							}
+						});
 
-                    }
-                }
-            },
-            dom: mr.dt.buttonDom,
-            buttons: mr.dt.buttons,
-            order: mySort,
-            columnDefs: columnDefs,
-		    createdRow: function( nRow, aData, iDataIndex ) {
-	        	// Update name in first column to link
-	        	var name=$('td:eq(0)', nRow).html();
-	        	if(name == ''){name = "No Name"};
-	        	var sn=$('td:eq(1)', nRow).html();
-	        	var link = mr.getClientDetailLink(name, sn, '#tab_usb-tab');
-	        	$('td:eq(0)', nRow).html(link);
+					}
+				}
+			},
+			dom: mr.dt.buttonDom,
+			buttons: mr.dt.buttons,
+			order: mySort,
+			columnDefs: columnDefs,
+			createdRow: function( nRow, aData, iDataIndex ) {
+				// Update name in first column to link
+				var name=$('td:eq(0)', nRow).html();
+				if(name == ''){name = "No Name"};
+				var sn=$('td:eq(1)', nRow).html();
+				var link = mr.getClientDetailLink(name, sn, '#tab_usb-tab');
+				$('td:eq(0)', nRow).html(link);
 
-	        	// Internal/External Bus
-	        	var internal=$('td:eq(7)', nRow).html();
-	        	internal = internal == '1' ? i18n.t('internal') :
-	        	(internal === '0' ? i18n.t('external') : '')
-	        	$('td:eq(7)', nRow).html(internal)
+				// Internal/External Bus
+				var internal=$('td:eq(7)', nRow).html();
+				internal = internal == '1' ? i18n.t('internal') :
+				(internal === '0' ? i18n.t('external') : '')
+				$('td:eq(7)', nRow).html(internal)
 
-	        	// Media
-	        	var media=$('td:eq(8)', nRow).html();
-	        	media = media == '1' ? i18n.t('yes') :
-	        	(media === '0' ? i18n.t('no') : '')
-	        	$('td:eq(8)', nRow).html(media)
-		    }
-	    });
+				// Media
+				var media=$('td:eq(8)', nRow).html();
+				media = media == '1' ? i18n.t('yes') :
+				(media === '0' ? i18n.t('no') : '')
+				$('td:eq(8)', nRow).html(media)
+			}
+		});
 
 	});
 </script>
